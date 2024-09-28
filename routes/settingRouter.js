b90ef12869d6517eeb805b1bced0319af59bf558
Dharma Putra <dharmaputra250@gmail.com>
2024-09-28 09:16:41 +0700

    update authorized

--- routes/settingRouter.js
+++ routes/settingRouter.js
@@ -1,49 +1,53 @@
-import { Router } from 'express'
-
+import { Router } from "express";
 
 const router = Router();
 
 import {
-    getAllJurusanProdi,
-    getJurusanProdi,
-    createJurusanProdi,
-    updateJurusanProdi,
-    deleteJurusanProdi,
-    createSettings,
-    getAllSettings,
-    getSetting,
-    updateSetting,
-    deleteSetting
-} from '../controllers/settingsController.js';
+  getAllJurusanProdi,
+  getJurusanProdi,
+  createJurusanProdi,
+  updateJurusanProdi,
+  deleteJurusanProdi,
+  createSettings,
+  getAllSettings,
+  getSetting,
+  updateSetting,
+  deleteSetting,
+} from "../controllers/settingsController.js";
 
 import {
-    getAllMeja,
-    createMeja,
-    getMeja,
-    updateMeja,
-    deleteMeja
-} from '../controllers/MejaRegistrasiController.js';
-import { authorizedPermissionsSettings } from '../middleware/authMiddleware.js';
-
+  getAllMeja,
+  createMeja,
+  getMeja,
+  updateMeja,
+  deleteMeja,
+} from "../controllers/MejaRegistrasiController.js";
+import { authorizedPermissionsSettings } from "../middleware/authMiddleware.js";
 
 // router.route('/').get(getAllJurusanProdi).post(createJurusanProdi);
 // settings
-router.route('/')
-    .get(getAllSettings)
-    .post(authorizedPermissionsSettings('superadmin'), createSettings);
+router
+  .route("/")
+  .get(getAllSettings)
+  .post(authorizedPermissionsSettings("superadmin"), createSettings);
 
-router.route('/meja').get(getAllMeja).post(createMeja);
+router
+  .route("/meja")
+  .get(getAllMeja)
+  .post(authorizedPermissionsSettings("superadmin"), createMeja);
 
-router.route('/:id')
-    // .all(authorizedPermissionsSettings('superadmin'))
-    .get(getSetting)
-    .patch(authorizedPermissionsSettings('superadmin'),updateSetting)
-    .delete(authorizedPermissionsSettings('superadmin'),deleteSetting);
+router
+  .route("/:id")
+  // .all(authorizedPermissionsSettings('superadmin'))
+  .get(getSetting)
+  .patch(authorizedPermissionsSettings("superadmin"), updateSetting)
+  .delete(authorizedPermissionsSettings("superadmin"), deleteSetting);
 
-router.route('/meja/:id')
-    .get(getMeja)
-    .patch(updateMeja)
-    .delete(deleteMeja);
+router
+  .route("/meja/:id")
+  .get(getMeja)
+  .patch(authorizedPermissionsSettings("superadmin"), updateMeja)
+  .delete(authorizedPermissionsSettings("superadmin"), deleteMeja);
 
 // router.route('/:id')
 //     .get(getJurusanProdi)
